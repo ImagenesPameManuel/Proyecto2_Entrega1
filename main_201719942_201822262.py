ead6de4528e14609df29d2a9e29ebe7729ebe4cc
pamela-rg022 <p.ramirezg@uniandes.edu.co>
2021-02-25 08:27:24 -0500

    imagenes

--- main_201719942_201822262.py
+++ main_201719942_201822262.py
@@ -2,3 +2,20 @@
 #Manuel Gallegos Bustamante #Código: 201719942
 #Análisis y procesamiento de imágenes: Proyecto2 Entrega1
 #Se importan librerías que se utilizarán para el desarrollo del laboratorio
+from skimage.filters import threshold_otsu
+import nibabel
+from scipy.io import loadmat
+import os
+import glob
+import numpy as np
+import skimage.io as io
+import requests
+from  scipy.signal import correlate2d
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+
+def MyCCorrelation_201719942_201822262(image, kernel, boundary_condition):
+    CCorrelation=0
+    return CCorrelation
+
+prueba_scipy=correlate2d()
